4c51818a9eba9b1d3c9bc29f52043b5f35f4f025
David Hou <david.hou314@gmail.com>
2015-04-08 13:49:24 -0700

    Login -> problems and fix a copy pasta derp

--- views/pages/problems.py
+++ views/pages/problems.py
@@ -9,5 +9,5 @@ app = Blueprint('page_problems', __name__)
 
 @app.route('/problems', methods=['GET', 'POST'])
 @login_required
-def page_scoreboard():
+def page_problems():
     return render_template('problems.html', problems=api.problems.get_all_problems(), use_hints=config.platform.USE_HINTS)

8f569181ad13d1eef82169d877e60a27c9677a21
David Hou <david.hou314@gmail.com>
2015-04-09 13:21:03 -0700

    Use a global var and if/else to determine page for navbar

--- tools/pages.py
+++ tools/pages.py
@@ -6,15 +6,18 @@ from flask_login import current_user
 import config
 
 
-def set_up_globals():
+def set_up_globals(current_page):
     g.ctf_name = config.platform.CTF_NAME
     g.show_problems_unauthed = config.platform.PROBLEMS_AVAILABLE_WITHOUT_LOGIN
+    g.current_page = current_page
     g.authed = current_user.is_authenticated()
 
 # Wrapper to set up globals
-def page(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        set_up_globals()
-        return func(*args, **kwargs)
-    return wrapper
+def page(current_page):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            set_up_globals(current_page)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
--- views/pages/main_page.py
+++ views/pages/main_page.py
@@ -6,6 +6,6 @@ import tools
 app = Blueprint('page_main', __name__)
 
 @app.route('/', methods=['GET', 'POST'])
-@tools.pages.page
+@tools.pages.page('main')
 def page_main():
     return render_template('main_page.html', team_count=str(api.stats.team_count(0)['data']['count']))

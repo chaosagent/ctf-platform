7e1464483c4b984001e97ad5deef7227cecaa2b5
David Hou <david.hou314@gmail.com>
2015-04-09 13:07:14 -0700

    Add authed vs unauthed navbars

--- tools/pages.py
+++ tools/pages.py
@@ -1,12 +1,15 @@
 from functools import wraps
 
 from flask import g
+from flask_login import current_user
 
 import config
 
 
 def set_up_globals():
     g.ctf_name = config.platform.CTF_NAME
+    g.show_problems_unauthed = config.platform.PROBLEMS_AVAILABLE_WITHOUT_LOGIN
+    g.authed = current_user.is_authenticated()
 
 # Wrapper to set up globals
 def page(func):

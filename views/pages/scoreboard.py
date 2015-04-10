a515c0b05a203e160a71a7e90d93a6c8a8a25a26
David Hou <david.hou314@gmail.com>
2015-04-09 22:24:02 -0700

    Allow scoreboard to be seen before contest start

--- views/pages/scoreboard.py
+++ views/pages/scoreboard.py
@@ -8,6 +8,5 @@ app = Blueprint('page_scoreboard', __name__)
 
 @app.route('/scoreboard', methods=['GET', 'POST'])
 @tools.pages.page('scoreboard')
-@tools.pages.competition_started_required
 def page_scoreboard():
     return render_template('scoreboard.html', teams=api.stats.scoreboard()['data'])
